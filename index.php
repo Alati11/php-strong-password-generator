b327d9991f483af8f35a1d239f643931d4276153
Alati11 <peppealati@gmail.com>
2023-11-23 15:45:01 +0100

    add function random_password

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+function random_password( $length = 8 ) {
+    $caratteri = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+    $password = substr( str_shuffle( $caratteri ), 0, $length );
+    return $password;
+}
+var_dump(random_password());
+?>  
 
+<h1>Genera Password</h1>
 
+<form action="" method="GET">
+  <input type="submit" value="genera">
+  <h2><?php echo random_password() ?></h2>
+</form>
 
-?>
-<h1>Index </h1>
