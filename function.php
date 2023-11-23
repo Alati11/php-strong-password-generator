8fc1cdab6813010fa71ff755d591e77e1dacb29d
Alati11 <peppealati@gmail.com>
2023-11-23 16:18:25 +0100

    include file function.php

--- function.php
+++ function.php
@@ -1,7 +1,8 @@
 <?php
-
-
-
-?>
-
-<h1>password</h1>
+function random_password( $length = 8 ) {
+    $caratteri = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+    $password = substr( str_shuffle( $caratteri ), 0, $length );
+    return $password;
+}
+//var_dump(random_password());
+?>  
--- index.php
+++ index.php
@@ -1,11 +1,9 @@
+
 <?php
-function random_password( $length = 8 ) {
-    $caratteri = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
-    $password = substr( str_shuffle( $caratteri ), 0, $length );
-    return $password;
-}
-var_dump(random_password());
-?>  
+
+include __DIR__ . '/function.php';
+
+?>
 
 <h1>Genera Password</h1>
 
