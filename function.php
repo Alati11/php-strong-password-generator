33f50a9a19565484b2cc0a3e98d0ccdd13c8ea44
Alati11 <peppealati@gmail.com>
2023-11-23 17:41:33 +0100

    correction length password

--- function.php
+++ function.php
@@ -1,9 +1,10 @@
 <?php
-function random_password( $length = 8 ) {
+function random_password( $length = 8) {
     $caratteri = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
     $password = substr( str_shuffle( $caratteri ), 0, $length );
     return $password;
+    
 }
 
-var_dump(random_password());
+// var_dump(random_password());
 ?>  
--- index.php
+++ index.php
@@ -3,15 +3,21 @@
 
 include __DIR__ . '/function.php';
 
+// var_dump(isset($_GET['length']));
+// var_dump($_GET['length']);
+
+$valid_password = $_GET['length']; 
+
 ?>
 
 <h1>Genera Password</h1>
 
+<!-- <form action="funtion.php" method="GET"> -->
 <form action="" method="GET">
-    <input type="number" name="$length" placeholder="lunghezza password">
+    <input type="number" name="length" placeholder="lunghezza password">
     <input type="submit" value="genera"> 
 </form>
 
-<h2><?php echo random_password() ?></h2>
+<h2><?php echo random_password($valid_password) ?></h2>
 
 
